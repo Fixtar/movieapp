88b497c1dfad5fa7a2f936357e0fd416a9952aac
seongha_h <58902013+Fixtar@users.noreply.github.com>
2022-02-11 23:49:58 +0900

    npm version update, todo리스트 리액트로 틀잡기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+
+    setToDos(currentArray => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>MY ToDos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder='할일을 등록하세요' />
+        <button>추가</button>
+      </form>
     </div>
   );
 }
